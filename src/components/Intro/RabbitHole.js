86abc1e6780e5e6b9f08a388c582e65ec62b2ef6
Jennifer Tendell <jennifer.tendell@gmail.com>
2021-12-23 01:33:04 +0100

    #21 Add gradient color for rabbitHole.

--- src/components/Intro/RabbitHole.js
+++ src/components/Intro/RabbitHole.js
@@ -1,13 +1,16 @@
+import { GradientTexture } from "@react-three/drei";
 import React from "react";
 
 export default function RabbitHole() {
-  function Circle() {
-    return (
-      <mesh position={[0, -0.5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-        <circleGeometry args={[5, 30]} />
-        <meshBasicMaterial color="#141514" />
-      </mesh>
-    );
-  }
-  return <Circle />;
+  return (
+    <mesh position={[0, -0.5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <circleGeometry args={[5, 30]} />
+      <meshBasicMaterial>
+        <GradientTexture
+          stops={[0, 0.4, 0.6, 1]}
+          colors={["#EEF20A", "#F2420A", "#F20A34", "#EE0AF2"]}
+        />
+      </meshBasicMaterial>
+    </mesh>
+  );
 }
