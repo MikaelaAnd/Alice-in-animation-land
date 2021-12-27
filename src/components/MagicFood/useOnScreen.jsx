8af8a2c5a5ab45aab77124c61a8017a6719a3808
Felicia von Braun Wallin <feliciavonbraun@hotmail.com>
2021-12-27 15:05:53 +0100

    #30 added in viewport function

---
+++ src/components/MagicFood/useOnScreen.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react"
+
+export default function useOnScreen(ref) {
+
+  const [isIntersecting, setIntersecting] = useState(false)
+
+  useEffect(() => {
+      const observer = new IntersectionObserver(
+        ([entry]) => setIntersecting(entry.isIntersecting)
+      )
+    observer.observe(ref.current)
+    // Remove the observer as soon as the component is unmounted
+    return () => { observer.disconnect() }
+  }, [ref])
+
+  return isIntersecting
+}
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
