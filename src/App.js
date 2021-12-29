2d47349d102b4be3686bd407e3166e6bcdd43882
Felicia von Braun Wallin <feliciavonbraun@hotmail.com>
2021-12-29 21:32:09 +0100

    #100 alice and background illustated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import GlobalFonts from './fonts/fonts';
+import GlobalFonts from "./fonts/fonts";
 import Intro from "./components/Intro/Intro";
 import AliceFalling from "./components/AliceFalling/AliceFalling";
 import MagicFood from "./components/MagicFood/MagicFood";
+import SingingFlowers from "./components/SingingFlowers/SingingFlowers";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Intro />
       <AliceFalling />
       <MagicFood />
+      <SingingFlowers />
     </>
   );
 }
