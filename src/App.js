e132731dcf755def0cafb8d94738037f7ae10b2a
Mikaela Andersson <mikaela.andersson@stim.se>
2021-12-30 12:03:01 +0100

    changed AliceFalling.jsx to index.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import GlobalFonts from "./fonts/fonts";
 import Intro from "./components/Intro/Intro";
-import AliceFalling from "./components/AliceFalling/AliceFalling";
+import AliceFalling from "./components/AliceFalling";
 import MagicFood from "./components/MagicFood/MagicFood";
 import { TeaParty } from "./components/TeaParty";
 
