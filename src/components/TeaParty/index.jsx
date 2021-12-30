751297b5ced9c7585dc1b146af5bd942c33bb1b5
Mikaela Andersson <mikaela.andersson@stim.se>
2021-12-30 12:04:46 +0100

    #47 - illustrated Mad Hatter

--- src/components/TeaParty/index.jsx
+++ src/components/TeaParty/index.jsx
@@ -1,7 +1,7 @@
 import { Background } from "./styles";
 import { Canvas } from "@react-three/fiber";
-
 import { Sky, Stars } from "@react-three/drei";
+import MadHatter from "./MadHatter.gif";
 
 // PLANE SOM BORD FÖR ATT SKY SER UT SOM HIMMEL
 
@@ -11,7 +11,12 @@ export function TeaParty() {
       <Canvas camera={{ position: [0, 0, 0], fov: 80 }}>
         <Sky />
         <Stars />
+        <ambientLight />
+        <Box1 />
       </Canvas>
+      <div>
+        <img src={MadHatter} alt="" style={{ width: "20rem"}} />
+      </div>
     </Background>
   );
 }
@@ -20,7 +25,7 @@ export const Box1 = () => {
   return (
     <mesh position={[0, -13, 0]}>
       <cylinderGeometry args={[30, 30, 3, 30]} />
-      <meshLambertMaterial color={"blue"} />
+      <meshLambertMaterial color={"#CA9088"} />
     </mesh>
   );
 };
