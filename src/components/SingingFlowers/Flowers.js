d12413bb70109d44d40f8a278b48778c7271b5f0
Felicia von Braun Wallin <feliciavonbraun@hotmail.com>
2022-01-10 12:03:51 +0100

    #31 flowers positioned and swaying

--- src/components/SingingFlowers/Flowers.js
+++ src/components/SingingFlowers/Flowers.js
@@ -8,21 +8,43 @@ import Trumpet1 from "./assets/trumpet1.png";
 import Trumpet2 from "./assets/trumpet2.png";
 import Flower1 from "./assets/flower1.png";
 import Flower2 from "./assets/flower2.png";
-// import Flower3 from "./assets/flower3.png";
+import Flower3 from "./assets/flower3.png";
+import Flower4 from "./assets/flower4.png";
+import Dogflower from "./assets/dogflower.png";
 
 function Flowers() {
   return (
     <FlowerContent>
-      <img src={Alice} alt="" height="50%" />
+      <img src={Alice} alt="Alice sitting on a leaf" height="50%" />
       <Miniflowers>
-        <img src={Trumpet1} alt="" height="100%" />
-        <img src={Trumpet2} alt="" height="100%" />
-        <img src={Flower1} alt="" height="100%" />
-        <img src={Flower2} alt="" height="100%" />
-        {/* <img src={Flower3} alt="" height="100%" /> */}
+        <JammingFlower
+          src={Trumpet1}
+          alt="Jamming trumpet flower"
+          height="65%"
+          bottom={15}
+          left={25}
+        />
+        <JammingFlower
+          src={Trumpet2}
+          alt="Jamming trumpet flower"
+          height="65%"
+          bottom={15}
+          left={30}
+        />
+        <JammingFlower
+          src={Dogflower}
+          alt="Three dog flowers jamming"
+          height="65%"
+          bottom={15}
+          left={40}
+        />
+        <JammingFlower src={Flower1} alt="" height="30%" bottom={7} left={30} />
+        <JammingFlower src={Flower2} alt="" height="30%" bottom={7} left={35} />
+        <JammingFlower src={Flower3} alt="" height="30%" bottom={7} left={43} />
+        <JammingFlower src={Flower4} alt="" height="30%" bottom={7} left={48} />
       </Miniflowers>
       <RoseDiv>
-        <img src={Rose} alt="" height="100%" />
+        <img src={Rose} alt="A big rose singing" height="100%" />
       </RoseDiv>
     </FlowerContent>
   );
@@ -32,38 +54,56 @@ export default Flowers;
 const FlowerContent = styled.div`
   display: flex;
   align-items: flex-end;
+  justify-content: space-between;
   width: 100%;
   height: 50%;
   z-index: 1;
-  /* background: blue; */
+  position: relative;
 `;
 
 const RoseDiv = styled.div`
   height: 100%;
-  /* background: red; */
 `;
 
-const move = keyframes`
-  0%{
+const moveRight = keyframes`
+  0%, 100% {
     transform: rotateZ(-3deg);
     transform-origin: bottom;
   }
   50%{
     transform: rotateZ(3deg);
+  }
+`;
+
+const moveLeft = keyframes`
+  0%, 100% {
+    transform: rotateZ(3deg);
     transform-origin: bottom;
   }
-  100%{
+  50%{
     transform: rotateZ(-3deg);
-    transform-origin: bottom;
   }
 `;
 
 const Miniflowers = styled.div`
   display: flex;
   height: 50%;
-  /* background: pink; */
 
-  > img {
-    animation: ${move} 2s infinite;
+  > img:nth-child(1),
+  > img:nth-child(3),
+  > img:nth-child(4) {
+    animation: ${moveRight} 2s infinite;
   }
+  > img:nth-child(2),
+  > img:nth-child(5),
+  > img:nth-child(6),
+  > img:nth-child(7) {
+    animation: ${moveLeft} 2s infinite;
+  }
+`;
+
+const JammingFlower = styled.img`
+  position: absolute;
+  bottom: ${(props) => props.bottom}%;
+  left: ${(props) => props.left}%;
 `;
