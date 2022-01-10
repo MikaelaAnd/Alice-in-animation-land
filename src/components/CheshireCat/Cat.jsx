5f7507ac2ce97685c53c427de9bc65d8073ecf8d
Jennifer Tendell <jennifer.tendell@gmail.com>
2022-01-10 11:21:14 +0100

    #34 Add cat and tree.

---
+++ src/components/CheshireCat/Cat.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled, { keyframes } from "styled-components";
+import catStripes from "./images/cat-stripes.png";
+import cat from "./images/cat.png";
+
+export default function Cat() {
+    return (
+        <>
+          <CatStriped src={catStripes} />
+          <CatFill src={cat} />
+        </>
+    )
+};
+
+const Container = styled.div`
+  position: absolute;
+  margin-left: auto; 
+  margin-right: auto;
+  width: 50%;
+  max-width: 60rem;
+`;
+
+const showCatStripes = keyframes`
+  0% { clip-path: polygon(80% 95%, 80% 93%, 80% 93%, 80% 100%, 80% 100%); }
+  15% { clip-path: polygon(80% 95%, 80% 93%, 80% 93%, 80% 100%, 80% 100%); }
+  20% { clip-path: polygon(70% 95%, 80% 93%, 80% 93%, 80% 100%, 70% 100%); }
+  25% { clip-path: polygon(77% 93%, 79% 90%, 80% 90%, 80% 100%, 65% 100%); }
+  30% { clip-path: polygon(65% 90%, 79% 90%, 80% 90%, 80% 100%, 65% 100%); }
+  35% { clip-path: polygon(65% 90%, 79% 90%, 81% 90%, 80% 100%, 65% 100%); }
+  40% { clip-path: polygon(58% 90%, 60% 80%, 81% 85%, 80% 100%, 65% 100%); }
+  45% { clip-path: polygon(55% 75%, 83% 88%, 87% 85%, 80% 100%, 65% 100%); }
+  50% { clip-path: polygon(55% 75%, 70% 68%, 90% 82%, 80% 100%, 65% 100%); }
+  55% { clip-path: polygon(70% 66%, 92% 81%, 100% 70%, 80% 100%, 40% 100%); }
+  60% { clip-path: polygon(70% 66%, 73% 62%, 100% 65%, 90% 100%, 40% 100%); }
+  65% { clip-path: polygon(73% 62%, 95% 64%, 100% 53%, 90% 100%, 40% 100%); }
+  70% { clip-path: polygon(72% 63%, 72% 59%, 92% 45%, 100% 100%, 40% 100%); }
+  75% { clip-path: polygon(71% 63%, 74% 48%, 92% 45%, 100% 100%, 43% 100%); }
+  100% { clip-path: polygon(0% 0%, 74% 0%, 92% 45%, 100% 100%, 0% 100%); }
+`
+
+const CatStriped = styled.img`
+  position: absolute;
+  width: 50%;
+  max-width: 60rem;
+  animation: ${showCatStripes} 5s ease-in-out;
+`;
+
+const showCat = keyframes`
+  0% { opacity: 0}
+  90% { opacity: 0}
+  100% { opacity: 1}
+`
+
+const CatFill = styled.img`
+  position: absolute;
+  width: 50%;
+  max-width: 60rem;
+  animation: ${showCat} 6s linear;
+`;
--- src/components/CheshireCat/index.jsx
+++ src/components/CheshireCat/index.jsx
@@ -1,8 +1,9 @@
 import React, { useRef } from "react";
-import styled, { keyframes } from "styled-components";
-import catStripes from "./images/cat-stripes.png";
-import cat from "./images/cat.png";
+import styled from "styled-components";
 import useOnScreen from "../../useOnScreen.jsx";
+import Cat from "./Cat.jsx";
+import branch from "./images/branch.png";
+import stem from "./images/stem.png";
 
 export default function CheshireCat() {
   const a = useRef();
@@ -10,61 +11,44 @@ export default function CheshireCat() {
 
   return (
     <Container ref={a}>
-      {isVisible  &&
-        <>
-          <CatStriped src={catStripes} />
-          <Cat src={cat} />
-        </>
-      }
+      <Stem>
+        <StemImage src={stem} />
+      </Stem>
+      <Branch>
+        <BranchImage src={branch} />
+      </Branch>
+      {isVisible && <Cat />}
     </Container>
   );
 }
 
 const Container = styled.div`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
-  top: 0;
-  left: 0;
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(200deg, #043a59, #010d18);
 `;
 
-const showCatStripes = keyframes`
-  0% { clip-path: polygon(80% 95%, 80% 93%, 80% 93%, 80% 100%, 80% 100%); }
-  15% { clip-path: polygon(80% 95%, 80% 93%, 80% 93%, 80% 100%, 80% 100%); }
-  20% { clip-path: polygon(70% 95%, 80% 93%, 80% 93%, 80% 100%, 70% 100%); }
-  25% { clip-path: polygon(77% 93%, 79% 90%, 80% 90%, 80% 100%, 65% 100%); }
-  30% { clip-path: polygon(65% 90%, 79% 90%, 80% 90%, 80% 100%, 65% 100%); }
-  35% { clip-path: polygon(65% 90%, 79% 90%, 81% 90%, 80% 100%, 65% 100%); }
-  40% { clip-path: polygon(58% 90%, 60% 80%, 81% 85%, 80% 100%, 65% 100%); }
-  45% { clip-path: polygon(55% 75%, 83% 88%, 87% 85%, 80% 100%, 65% 100%); }
-  50% { clip-path: polygon(55% 75%, 70% 68%, 90% 82%, 80% 100%, 65% 100%); }
-  55% { clip-path: polygon(70% 66%, 92% 81%, 100% 70%, 80% 100%, 40% 100%); }
-  60% { clip-path: polygon(70% 66%, 73% 62%, 100% 65%, 90% 100%, 40% 100%); }
-  65% { clip-path: polygon(73% 62%, 95% 64%, 100% 53%, 90% 100%, 40% 100%); }
-  70% { clip-path: polygon(72% 63%, 72% 59%, 92% 45%, 100% 100%, 40% 100%); }
-  75% { clip-path: polygon(71% 63%, 74% 48%, 92% 45%, 100% 100%, 43% 100%); }
-  100% { clip-path: polygon(0% 0%, 74% 0%, 92% 45%, 100% 100%, 0% 100%); }
-`
-
-const CatStriped = styled.img`
+const Stem = styled.div`
   position: absolute;
-  width: 50%;
-  max-width: 60rem;
-  animation: ${showCatStripes} 5s ease-in-out;
+  width: 100%;
+  height: 100%;
 `;
 
-const showCat = keyframes`
-  0% { opacity: 0}
-  90% { opacity: 0}
-  100% { opacity: 1}
-`
+const StemImage = styled.img`
+  width: 100%;
+  height: 100%;
+`;
 
-const Cat = styled.img`
+const Branch = styled.div`
   position: absolute;
-  width: 50%;
-  max-width: 60rem;
-  animation: ${showCat} 6s linear;
+  width: 100%;
+  margin-top: 4.5rem;
+`;
+
+const BranchImage = styled.img`
+  width: 100%;
 `;
