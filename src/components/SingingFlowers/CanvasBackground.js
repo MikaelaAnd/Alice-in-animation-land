2242e83cec4c2835d4fa4075863c26e0df0489fb
Felicia von Braun Wallin <feliciavonbraun@hotmail.com>
2022-01-11 11:58:25 +0100

    #113 changes background

--- src/components/SingingFlowers/CanvasBackground.js
+++ src/components/SingingFlowers/CanvasBackground.js
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 import { Canvas } from "@react-three/fiber";
-import React from "react";
-import Stem from "./Stem";
-import { OrbitControls } from "@react-three/drei";
+import React, { Suspense } from "react";
+// import Stem from "./Stem";
+import { Cloud } from "@react-three/drei";
 import {
   Center,
-  OrthographicCamera,
-  // PerspectiveCamera,
+  //   OrthographicCamera,
+  PerspectiveCamera,
 } from "@react-three/drei";
+import Loading from "../../Loading";
 
 function CanvasBackground() {
   // const TotalStems = () => {
@@ -21,19 +22,24 @@ function CanvasBackground() {
   }
   // }
 
+  // mouse
+  // let mouseX = 0;
+  // let mouseY = 0;
+  //     document.addEventListener("mousemove", (e) => {
+  //     mouseX = e.clientX;
+  //     mouseY = e.clientY;
+  //     });
+
   return (
     <CanvasContainer>
       <Canvas>
-        <OrbitControls />
-        <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={70} />
-        {/* <PerspectiveCamera makeDefault position={[0, 0, 10]} /> */}
-        <Center alignBottom>
-          <mesh>
-            {allStems.map((positionX, index) => (
-              <Stem x={positionX} key={index} />
-            ))}
-          </mesh>
-        </Center>
+        {/* <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={70} /> */}
+        <PerspectiveCamera makeDefault position={[3, 0, 10]} />
+        <Suspense fallback={<Loading />}>
+          <Center>
+            <Cloud position={[-4, -2, 0]} args={[3, 1]} />
+          </Center>
+        </Suspense>
       </Canvas>
     </CanvasContainer>
   );
@@ -41,10 +47,9 @@ function CanvasBackground() {
 export default CanvasBackground;
 
 const CanvasContainer = styled.div`
-  background: black;
+  background: #5a3552;
   position: absolute;
-  width: 100%;
-  z-index: 0;
   height: 100vh;
+  width: 100%;
   // {hovered ? aktuell stem = z-index: +1}
 `;
