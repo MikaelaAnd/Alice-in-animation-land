6340824ad30ed40b0bb77733bc7d1c994543c4a1
Felicia von Braun Wallin <feliciavonbraun@hotmail.com>
2022-01-13 15:07:31 +0100

    #113 removed stems

--- src/components/SingingFlowers/CanvasBackground.js
+++ src/components/SingingFlowers/CanvasBackground.js
@@ -1,22 +1,16 @@
 import styled from "styled-components";
 import { Canvas } from "@react-three/fiber";
 import React from "react";
-// import Stem from "./Stem";
 import { Cloud } from "@react-three/drei";
-import {
-  Center,
-  //   OrthographicCamera,
-  //   PerspectiveCamera,
-} from "@react-three/drei";
+import { Center, PerspectiveCamera } from "@react-three/drei";
 
 function CanvasBackground() {
   return (
     <CanvasContainer>
       <Canvas>
-        {/* <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={70} /> */}
-        {/* <PerspectiveCamera makeDefault position={[3, 0, 10]} /> */}
+        <PerspectiveCamera makeDefault position={[3, 0, 10]} />
         <Center>
-          <Cloud position={[-4, -2, 0]} args={[3, 2]} />
+          <Cloud position={[-4, -2, 0]} args={[3, 2]} color={"white"} />
         </Center>
       </Canvas>
     </CanvasContainer>
@@ -25,9 +19,8 @@ function CanvasBackground() {
 export default CanvasBackground;
 
 const CanvasContainer = styled.div`
-  background: #5a3552;
+  background: #808bed;
   position: absolute;
   height: 100vh;
   width: 100%;
-  // {hovered ? aktuell stem = z-index: +1}
 `;
