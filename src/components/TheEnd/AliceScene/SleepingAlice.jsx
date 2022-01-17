3248b22383123b86e227fc641ee5e70c82440b84
Jennifer Tendell <jennifer.tendell@gmail.com>
2022-01-17 17:16:54 +0100

    #151 Add background and platform.

--- src/components/TheEnd/AliceScene/SleepingAlice.jsx
+++ src/components/TheEnd/AliceScene/SleepingAlice.jsx
@@ -1,16 +1,18 @@
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import React from 'react'
 import styled from 'styled-components';
-import Ground from '../../Intro/Ground';
+import Grass from './Grass';
 
 export default function SleepingAlice() {
     return (
         <CanvasContainer>
-          <Canvas camera={{ position: [0, 30, 100]}}>
+          <Canvas camera={{ position: [0, 10, 100]}}>
               <OrbitControls />
-              <ambientLight intensity={3}/>
-              <Ground />
+              <Sky />
+              <ambientLight intensity={1}/>
+              <pointLight intensity={2} color={"white"}></pointLight>
+              <Grass />
           </Canvas>
         </CanvasContainer>
     )
