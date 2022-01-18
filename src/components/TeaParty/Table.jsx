eeaf3d7828c916f96c83bcb576489710c180d039
Mikaela Andersson <mikaela.andersson@stim.se>
2022-01-18 12:36:47 +0100

    fixed table and animation

--- src/components/TeaParty/Table.jsx
+++ src/components/TeaParty/Table.jsx
@@ -22,11 +22,17 @@ const Container = styled.div`
 const TableDiv = styled.div`
   background-color: #ca9088;
   width: 100%;
-  height: 38%;
-  border-top-left-radius: 100%;
-  border-top-right-radius: 100%;
+  height: 27%;
   display: flex;
   justify-content: center;
   align-items: center;
   z-index: 2;
+  padding-bottom: 2rem;
+
+  @media (max-width: 820px) {
+    height: 40%;
+  }
+  @media (max-width: 768px) {
+    height: 38%;
+  }
 `;
