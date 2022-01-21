81fd13a4f561f5fbbd5e7309a41005c39190f5f7
Jennifer Tendell <jennifer.tendell@gmail.com>
2022-01-21 11:11:45 +0100

    Code fix

--- src/components/Transitions/LeavesTransition.jsx
+++ src/components/Transitions/LeavesTransition.jsx
@@ -16,19 +16,19 @@ const Transition = styled.div`
   display: flex;
   align-items: flex-end;
   height: 28rem;
-  background: #0D4A22;
+  background: #0d4a22;
   z-index: 50;
   pointer-events: none;
   overflow: hidden;
 `;
 
 const BlackBox = styled.div`
-    position: relative;
-    display: flex;
+  position: relative;
+  display: flex;
   justify-content: center;
-    height: 10rem;
-    width: 100%;
-    background: black;
+  height: 10rem;
+  width: 100%;
+  background: black;
 `;
 
 const LeavesImage = styled.img`
@@ -39,4 +39,3 @@ const LeavesImage = styled.img`
   top: 100%;
   transform: translateY(-50%);
 `;
-
